a3c343f491f8186b99f6881603a3d131d892e72b
Ravindra <ravindrakaranam95@gmail.com>
2025-08-16 22:02:44 +0530

    added conflict to categories in sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,15 +1,15 @@
 -- Insert default categories
-INSERT INTO categories (name) VALUES ('Electronics');
-INSERT INTO categories (name) VALUES ('Clothing');
-INSERT INTO categories (name) VALUES ('Books');
+INSERT INTO categories (name) VALUES ('Electronics') ON CONFLICT (name) DO NOTHING;;
+INSERT INTO categories (name) VALUES ('Clothing') ON CONFLICT (name) DO NOTHING;;
+INSERT INTO categories (name) VALUES ('Books') ON CONFLICT (name) DO NOTHING;;
 
 
 -- Products
 
 
 INSERT INTO products (title, description, price, location, image_url, category_id)
-VALUES ('Sample Book', 'A great read', 29.99, 'New York', '/images/book.jpg', 3) ON CONFLICT DO NOTHING;
+VALUES ('Sample Book', 'A great read', 29.99, 'New York', '/images/book.jpg', 4) ON CONFLICT DO NOTHING;
 
 INSERT INTO products (title, description, price, location, image_url, category_id)
-VALUES ('mobile phone', 'High-quality phone apple iphone 16', 160000, 'San Francisco', '/images/phone.jpg', 1)
+VALUES ('mobile phone', 'High-quality phone apple iphone 16', 160000, 'San Francisco', '/images/phone.jpg', 2)
 ON CONFLICT (title) DO NOTHING;
