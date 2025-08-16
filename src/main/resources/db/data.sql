ad29930d1079b5a5808f50b01b0360de124a8b95
Ravindra <ravindrakaranam95@gmail.com>
2025-08-16 22:11:38 +0530

    changes in the data.sql file for products display

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,4 +1,5 @@
 -- Insert default categories
+
 INSERT INTO categories (name) VALUES ('Electronics') ON CONFLICT (name) DO NOTHING;;
 INSERT INTO categories (name) VALUES ('Clothing') ON CONFLICT (name) DO NOTHING;;
 INSERT INTO categories (name) VALUES ('Books') ON CONFLICT (name) DO NOTHING;;
