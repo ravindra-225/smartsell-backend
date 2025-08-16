2ad2e8ca9f9ddf66545009ad7dae49b6d90ed2d9
Ravindra <ravindrakaranam95@gmail.com>
2025-08-16 18:01:35 +0530

    application properties changed for render deployment

---
+++ src/main/resources/db/migration2.sql
@@ -0,0 +1,4 @@
+-- Insert default categories
+INSERT INTO categories (name) VALUES ('Electronics');
+INSERT INTO categories (name) VALUES ('Clothing');
+INSERT INTO categories (name) VALUES ('Books');
