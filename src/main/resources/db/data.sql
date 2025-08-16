a72fe3f474ff91927f5ab4296a4b7768d757d7bc
Ravindra <ravindrakaranam95@gmail.com>
2025-08-16 22:15:13 +0530

    added conflict to product title

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,16 +1,17 @@
 -- Insert default categories
+TRUNCATE TABLE categories RESTART IDENTITY CASCADE;
 
-INSERT INTO categories (name) VALUES ('Electronics') ON CONFLICT (name) DO NOTHING;;
-INSERT INTO categories (name) VALUES ('Clothing') ON CONFLICT (name) DO NOTHING;;
-INSERT INTO categories (name) VALUES ('Books') ON CONFLICT (name) DO NOTHING;;
+INSERT INTO categories (name) VALUES ('Electronics');
+INSERT INTO categories (name) VALUES ('Clothing');
+INSERT INTO categories (name) VALUES ('Books');
 
 
 -- Products
 
 
 INSERT INTO products (title, description, price, location, image_url, category_id)
-VALUES ('Sample Book', 'A great read', 29.99, 'New York', '/images/book.jpg', 4) ON CONFLICT DO NOTHING;
+VALUES ('Sample Book', 'A great read', 29.99, 'New York', '/images/book.jpg', 3) ON CONFLICT DO NOTHING;
 
 INSERT INTO products (title, description, price, location, image_url, category_id)
-VALUES ('mobile phone', 'High-quality phone apple iphone 16', 160000, 'San Francisco', '/images/phone.jpg', 2)
+VALUES ('mobile phone', 'High-quality phone apple iphone 16', 160000, 'San Francisco', '/images/phone.jpg', 1)
 ON CONFLICT (title) DO NOTHING;
